09054aa4f288602c92849cd772cb5903d4416273
lazherbh <lazherbenhassine@gmail.com>
2024-08-20 10:27:59 +0100

    style commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { ThemePalette } from '@angular/material/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['../custom-theme.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 
